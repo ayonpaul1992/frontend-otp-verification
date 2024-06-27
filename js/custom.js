b22162895923245f03dc1f28f567b8eca51cb072
ayonpaul0 <paulayon100@gmail.com>
2024-06-27 12:00:56 +0530

    My sixth javascript project practice commit

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,35 @@
-let stringVal = "";
-let btnsGroup = document.querySelectorAll(".btns");
-Array.from(btnsGroup).forEach((bttn)=>{
-    bttn.addEventListener("click",(e)=>{
-        if(e.target.innerHTML == "="){
-            try{
-                stringVal = eval(stringVal);
-                document.querySelector(".inpt_digit").value = stringVal;
-            }
-            catch(err){
-                document.querySelector(".inpt_digit").value = "Error";
-                stringVal = "";
-            }
-        } else if(e.target.innerHTML == "AC"){
-            stringVal = "";
-            document.querySelector(".inpt_digit").value = stringVal;
-        }else if(e.target.id == "sqrtBtn"){
-            stringVal = Math.sqrt(parseFloat(document.querySelector(".inpt_digit").value)).toString();
-            document.querySelector(".inpt_digit").value = stringVal;
-        }else if(e.target.innerHTML == "%"){
-            stringVal = (parseFloat(stringVal)/100).toString();
-            document.querySelector(".inpt_digit").value = stringVal;
-        }else if(e.target.classList.contains("mprtn")){
-            stringVal = (parseFloat(stringVal)*-1).toString();
-            document.querySelector(".inpt_digit").value = stringVal;
+const codes = document.querySelectorAll(".code");
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+    code.addEventListener("keydown", (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+            codes[idx].value = "";
+            setTimeout(() => {
+                if (codes[idx + 1]) {
+                    codes[idx + 1].focus();
+                }
+            });
+        } else if (e.key === "Backspace") {
+            setTimeout(() => {
+                if (codes[idx - 1]) {
+                    codes[idx - 1].focus();
+                }
+            });
         }
-        else{
-            console.log(e.target);
-            stringVal = stringVal+e.target.innerHTML;
-            document.querySelector(".inpt_digit").value = stringVal;
+    });
+
+    code.addEventListener("paste", (e) => {
+        e.preventDefault();
+        const pasteData = e.clipboardData.getData("text");
+        const pasteArray = pasteData.split("");
+        pasteArray.forEach((char, pasteIdx) => {
+            if (codes[idx + pasteIdx]) {
+                codes[idx + pasteIdx].value = char;
+            }
+        });
+        if (codes[idx + pasteArray.length]) {
+            codes[idx + pasteArray.length].focus();
         }
     });
 });
